05535d9bba2c389bd4e88d4b4189f81ff2ed1f1e
bandit <80446239+bandit1397@users.noreply.github.com>
2025-09-26 08:13:10 +0900

    Update patrolSchedule.js

--- patrolSchedule.js
+++ patrolSchedule.js
@@ -2,8 +2,7 @@ const patrolSchedules = {
   안전조치: [
   { id:"CV1(42호)", startDate:"2025-05-16", endDate:"2025-09-26", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"22:00", end:"23:00" }] },
   { id:"CV2(41호)", startDate:"2025-08-12", endDate:"2025-10-11", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"06:00", end:"07:00" },{ start:"21:00", end:"22:00" }] },
-  { id:"CV3(42호)", startDate:"2025-08-25", endDate:"2025-09-24", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"19:00", end:"20:00" }] },
-   
+ 
   { id:"CV5(42호)", startDate:"2025-09-11", endDate:"2025-10-10", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"00:00", end:"02:00" }] },
   { id:"CV6(43호)", startDate:"2025-06-08", endDate:"2025-10-07", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"22:00", end:"23:00" }] },
   { id:"CV7(42호)", startDate:"2025-09-03", endDate:"2025-10-02", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"18:00", end:"19:00" }] },
