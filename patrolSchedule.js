f4d28c80e4ea88e4ce94943dbdc70903ac225ffc
bandit <80446239+bandit1397@users.noreply.github.com>
2025-10-04 08:13:56 +0900

    Update patrolSchedule.js

--- patrolSchedule.js
+++ patrolSchedule.js
@@ -1,19 +1,19 @@
 const patrolSchedules = {
   안전조치: [
   
-  { id:"CV2(41호)", startDate:"2025-08-12", endDate:"2025-10-11", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"06:00", end:"07:00" },{ start:"21:00", end:"22:00" }] },
+  { id:"CV2(41호)", startDate:"2025-08-12", endDate:"2025-11-11", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"06:00", end:"07:00" },{ start:"21:00", end:"22:00" }] },
   { id:"CV3(41호)", startDate:"2025-09-28", endDate:"2025-10-27", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"14:00", end:"15:00" },{ start:"22:00", end:"23:00" }] },
  
   { id:"CV5(42호)", startDate:"2025-09-11", endDate:"2025-10-10", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"00:00", end:"02:00" }] },
-  { id:"CV6(43호)", startDate:"2025-06-08", endDate:"2025-10-07", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"22:00", end:"23:00" }] },
+  { id:"CV6(43호)", startDate:"2025-06-08", endDate:"2025-11-07", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"22:00", end:"23:00" }] },
    
   
-  { id:"CV9(41호)", startDate:"2025-07-31", endDate:"2025-09-18", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"05:00", end:"06:00" },{ start:"19:00", end:"20:00" }] },
+  { id:"CV9(41호)", startDate:"2025-07-31", endDate:"2025-10-18", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"05:00", end:"06:00" },{ start:"19:00", end:"20:00" }] },
     
-  { id:"CV10(43호)", startDate:"2025-08-29", endDate:"2025-09-28", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"20:00", end:"21:30" }] },
-  { id:"CV11(43호)", startDate:"2025-08-29", endDate:"2025-09-28", daysOfWeek:[1,5], timeRanges:[{ start:"07:00", end:"08:00" },{ start:"15:00", end:"16:00" }] },
-  { id:"CV11-1(43호)", startDate:"2025-08-29", endDate:"2025-09-28", daysOfWeek:[2,3,4], timeRanges:[{ start:"07:00", end:"08:00" },{ start:"16:00", end:"17:00" }] },
-  { id:"CV12(41호)", startDate:"2025-09-03", endDate:"2025-10-02", daysOfWeek:[1,2,3,4,5], timeRanges:[{ start:"18:00", end:"19:00" }] },
+  
+  { id:"CV11(43호)", startDate:"2025-08-29", endDate:"2025-10-28", daysOfWeek:[1,5], timeRanges:[{ start:"07:00", end:"08:00" },{ start:"15:00", end:"16:00" }] },
+  { id:"CV11-1(43호)", startDate:"2025-08-29", endDate:"2025-10-28", daysOfWeek:[2,3,4], timeRanges:[{ start:"07:00", end:"08:00" },{ start:"16:00", end:"17:00" }] },
+  { id:"CV12(41호)", startDate:"2025-09-09", endDate:"2025-11-08", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"07:00", end:"08:00" }] },
 
   ],
 };
