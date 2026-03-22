c55460727f74a286a4f1d891d2ae16c29eeed161
bandit <80446239+bandit1397@users.noreply.github.com>
2026-03-22 20:53:58 +0900

    Update patrolSchedule.js

--- patrolSchedule.js
+++ patrolSchedule.js
@@ -1,17 +1,22 @@
 const patrolSchedules = {
   안전조치: [
   
-  { id:"CV1(43호)", startDate:"2025-10-20", endDate:"2025-12-19", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"18:00", end:"19:00" }] },
+  { id:"CV1(41호)", startDate:"2026-03-05", endDate:"2026-04-04", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"00:01", end:"01:00" }] },
     
-  { id:"CV2(41호)", startDate:"2025-08-12", endDate:"2025-12-11", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"06:00", end:"07:00" },{ start:"21:00", end:"22:00" }] },
-   
-  { id:"CV4(42호)", startDate:"2025-11-03", endDate:"2026-01-02", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"19:00", end:"20:00" }] },
+  { id:"CV2(42호)", startDate:"2026-03-11", endDate:"2026-04-10", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"19:00", end:"20:00" }] },
+    
+  { id:"CV3(41호)", startDate:"2026-03-12", endDate:"2026-04-11", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"13:00", end:"14:00" },{ start:"17:00", end:"18:00" }] },
+
+  { id:"CV4(41호)", startDate:"2026-03-08", endDate:"2026-04-07", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"20:00", end:"21:00" },{ start:"22:00", end:"23:00" }] },
    
   
-  { id:"CV5(41호)", startDate:"2025-01-04", endDate:"2025-12-03", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"14:00", end:"15:00" },{ start:"19:00", end:"20:00" }] },
+  { id:"CV5(43호)", startDate:"2026-03-17", endDate:"2026-04-16", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"07:00", end:"08:00" },{ start:"20:00", end:"21:00" }] },
     
   
-  { id:"CV6(42호)", startDate:"2025-11-07", endDate:"2025-12-06", daysOfWeek:[0,2,4,6], timeRanges:[{ start:"13:00", end:"14:00" }] },
+  { id:"CV6(41호)", startDate:"2026-03-15", endDate:"2026-04-14", daysOfWeek:[0,1,2,3,4,5,6], timeRanges:[{ start:"20:00", end:"21:00" }] },
+    
+  { id:"CV7(41호)", startDate:"2026-03-20", endDate:"2026-04-19", daysOfWeek:[0,1,2,5,6], timeRanges:[{ start:"08:00", end:"09:00" },{ start:"19:00", end:"20:00" }] },
+
   
   ],
 };
